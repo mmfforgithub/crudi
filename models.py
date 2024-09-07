2244dc8da8e7bd2c78df627a8d8e2ac2d911b9aa
miranda <161040125+mmfforgithub@users.noreply.github.com>
2024-09-07 17:51:10 -0300

    Update models.py

--- models.py
+++ models.py
@@ -2,15 +2,15 @@ from database import db
 
 class Usuario(db.Model):
     __tablename__ = "usuario"
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    idade = db.Column(db.Integer)
+    id_doacao = db.Column(db.Integer, primary_key=True)
+    doacao = db.Column(db.String(100))
+    valor = db.Column(db.Float(10, 2))
+    data = db.Column(db.Date)
 
     def __init__(self, nome, email, idade):
-        self.nome = nome
-        self.email = email
-        self.idade = idade
+        self.doacao = doacao
+        self.valor = valor
+        self.data = data
 
     def __repr__(self):
-        return "<Usuario {}>".format(self.nome)
+        return "<Doação de {}>".format(self.doacao)
