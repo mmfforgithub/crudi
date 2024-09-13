0a8920a14deef32f2ee6202e7655058eedb89b92
mmfforgithub <miguelmifurtado@gmail.com>
2024-09-13 16:44:45 -0300

    aula

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 #drive://usuario:senha@servidor/banco_dados
-conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/flaskg1"
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/pointofview"
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/doacao')
 def doacao():
     d = Doacao.query.all()
     return render_template('doacao_lista.html', dados = d)
 
-@app.route('/doacao/add')
+@app.route('/add')
 def doacao_add():
     return render_template('doacao_add.html')
 
-@app.route('/doacao/save', methods=['POST'])
+@app.route('/save', methods=['POST'])
 def doacao_save():
     doador = request.form.get('doador')
     valor = request.form.get('valor')
@@ -36,30 +31,30 @@ def doacao_save():
         db.session.add(doacao)
         db.session.commit()
         flash('Doação cadastrada!')
-        return redirect ("/doacao")
+        return redirect ("/")
     else:
         flash("Preencha todos os campos!")
-        return redirect ("/doacao/add")
+        return redirect ("/add")
     
-@app.route('/doacao/remove/<int:id_doacao>')
+@app.route('/remove/<int:id_doacao>')
 def doacao_removo(id_doacao):
     if id_doacao > 0:
         doacao = Doacao.query.get(id_doacao)
         db.session.delete(doacao)
         db.session.commit()
         flash('Doação removida.')
-        return redirect('/doacao')
+        return redirect('/')
     else:
         flash('Caminho incorreto.')
-        return redirect('/doacao')
+        return redirect('/')
 
 
-@app.route('/doacao/edita/<int:id_doacao>')
+@app.route('/edita/<int:id_doacao>')
 def doacao_editar(id_doacao):
     doacao = Doacao.query.get(id_doacao)
     return render_template('doacao_editar.html', dados = doacao)
 
-@app.route('/doacao/editasave', methods = ['POST'])
+@app.route('/editasave', methods = ['POST'])
 def doacao_edita_save():
     id_doacao = request.form.get('id_doacao')
     doacao = request.form.get('doador')
@@ -72,10 +67,10 @@ def doacao_edita_save():
         usuario.idade = idade
         db.session.commit()
         flash('Dados atualizados.')
-        return redirect('/doacao')
+        return redirect('/')
     else:
         flash('Não há.')
-        return redirect('/doacao')
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -4,13 +4,13 @@ class Doacao(db.Model):
     __tablename__ = "doacao"
     id_doacao = db.Column(db.Integer, primary_key=True)
     doador = db.Column(db.String(100))
-    valor = db.Column(db.Float(10, 2))
+    valor = db.Column(db.Float(2))
     data = db.Column(db.Date)
 
-    def __init__(self, nome, email, idade):
+    def __init__(self, doador, valor, data):
         self.doador = doador
         self.valor = valor
         self.data = data
 
     def __repr__(self):
-        return "<Doação de {}>".format(self.doador)
+        return "<Doação de {}>".format(self.valor)
